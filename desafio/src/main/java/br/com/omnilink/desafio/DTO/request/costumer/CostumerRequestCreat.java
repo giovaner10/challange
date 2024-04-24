b5b1773f3774e7996f86344bf07ce0825c16f0ed
Giovane Avelino Tiburcio <giovane.tiburcio@cwi.com.br>
2024-04-24 19:03:47 -0300

    ajuste

--- desafio/src/main/java/br/com/omnilink/desafio/DTO/request/costumer/CostumerRequestCreat.java
+++ desafio/src/main/java/br/com/omnilink/desafio/DTO/request/costumer/CostumerRequestCreat.java
@@ -1,15 +1,15 @@
 package br.com.omnilink.desafio.DTO.request.costumer;
 
-import br.com.omnilink.desafio.enums.customer.CostumerType;
-import jakarta.persistence.Column;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.br.CNPJ;
 
+@Getter
+@Setter
+@Builder
 public record CostumerRequestCreat(
         @NotBlank String name,
         @NotBlank @CNPJ String cnpj,
