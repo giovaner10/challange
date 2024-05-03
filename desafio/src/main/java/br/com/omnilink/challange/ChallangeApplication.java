3d3ca7065b90fea8531b3891d6a5a2594dac25d9
giovane_tiburcio <giovane.tiburcio@omnilink.com.br>
2024-05-03 08:08:23 -0300

    adicao portas

--- desafio/src/main/java/br/com/omnilink/challange/ChallangeApplication.java
+++ desafio/src/main/java/br/com/omnilink/challange/ChallangeApplication.java
@@ -1,11 +1,8 @@
 package br.com.omnilink.challange;
 
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @SpringBootApplication
